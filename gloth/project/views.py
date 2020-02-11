9710f4e84da1b1e60f09ae2c28592b30cb2e5cb9
YVES <yves.tshimangakabidi@ynov.com>
2020-02-11 16:44:39 +0100

    Premier voir dernier

---
+++ gloth/project/views.py
@@ -0,0 +1,35 @@
+import os
+from werkzeug import secure_filename
+from flask import Flask, request, redirect, url_for, render_template, flash
+#from .utils import treatment
+
+app = Flask(__name__)
+app.config.from_object("project.config.Config")
+
+from .forms import PatientForm, MedicForm
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    form = PatientForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            flash("All fields are required.")
+            return render_template("index.html", title="Gloth", subtitle="test", patient_form=form, name="Ynov")
+        else:
+            data = request.args.get(form)
+            return redirect(url_for('medic'), data=data)
+
+    return render_template("index.html", title="Gloth", subtitle="subtitle", patient_form=form, name="Ynov")
+
+
+@app.route('/medic', methods=["GET","POST"])
+def medic():
+
+    form = MedicForm(request.form)
+    patho_id = (request.form.get("pathology"))
+    user_id = (request.form.get("user"))
+    # Doit permettre d'envoyer l'id de la pathologie pour récupérer le(s) cis de celle-ci
+    #list = treatment(patho_id)
+    return render_template("medic.html", name="Ynov", pathology=patho_id, user=user_id) #, listTreatement=list)
